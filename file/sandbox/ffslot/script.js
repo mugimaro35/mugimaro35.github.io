61a7d56727b0b23db15cd8cd347269c251e37c15
mugimaro35 <chi_roru0530@yahoo.co.jp>
2026-08-16 00:48:01 +0900

    no message

--- file/sandbox/ffslot/script.js
+++ file/sandbox/ffslot/script.js
@@ -7,8 +7,7 @@ const images = [
     "file/sandbox/ffslot/image02.png",
     "file/sandbox/ffslot/image03.png",
     "file/sandbox/ffslot/image04.png",
-    "file/sandbox/ffslot/image05.png",
-    "file/sandbox/ffslot/image06.png"
+    "file/sandbox/ffslot/image05.png"
 ];
 
 const reels = [
